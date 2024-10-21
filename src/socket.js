7084bdec6c90d69c6828ca73a5dc55349fd2e7c4
Trực Nguyễn Hữu <truc95520@gmail.com>
2024-10-21 09:52:52 +0700

    Minh DC

--- src/socket.js
+++ src/socket.js
@@ -3,25 +3,26 @@ import { io } from "socket.io-client";
 
 export const state = reactive({
   connected: false,
+  fooEvents: [],
+  barEvents: [],
 });
 
 const URL = import.meta.env.VITE_SERVER_URL;
-//
-export const socket = io(URL, {
-  withCredentials: true,
-  transports: ["websocket"],
-});
+
+export const socket = io(URL);
 
 socket.on("connect", () => {
   state.connected = true;
-  console.log("Socket connected to server");
 });
 
 socket.on("disconnect", () => {
   state.connected = false;
-  console.log("Socket disconnected from server");
 });
 
-socket.on("connect_error", (err) => {
-  console.error("Socket connection error:", err);
+socket.on("foo", (...args) => {
+  state.fooEvents.push(args);
+});
+
+socket.on("bar", (...args) => {
+  state.barEvents.push(args);
 });
